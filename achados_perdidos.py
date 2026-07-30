d1b47e63d0a37dc14d346f19c142c7eab29e8e5d
erickawahigashi-boop <eric.kawahigashi@aluno.senai.br>
2026-07-30 16:25:16 -0300

    Add menu display function for lost and found

---
+++ achados_perdidos.py
@@ -0,0 +1,8 @@
+def mostrar_menu():
+print("\n================================")
+print(" ACHADOS E PERDIDOS DA ESCOLA")
+print("================================")
+print("1 - Cadastrar objeto")
+print("2 - Listar objetos")
+print("3 - Excluir objeto")
+print("4 - Sair")
