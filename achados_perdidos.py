f183d15fe181a24d4e3c3f5b67dd05eb77de4194
erickawahigashi-boop <eric.kawahigashi@aluno.senai.br>
2026-07-30 16:47:18 -0300

    Update achados_perdidos.py

--- achados_perdidos.py
+++ achados_perdidos.py
@@ -1,7 +1,7 @@
 def mostrar_menu():
   print("\n================================")
   print(" ACHADOS E PERDIDOS DA ESCOLA")
-  print("================================")
+  print("Boas-Vindas")
   print("1 - Cadastrar objeto")
   print("2 - Listar objetos")
   print("3 - Excluir objeto")
